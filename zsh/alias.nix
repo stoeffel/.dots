ef6bb2e5d386c6d077a49a9c6c2546a78510a8a1
Stoeffel <schtoeffel@gmail.com>
2020-04-17 21:11:51 +0200

    backup

--- zsh/alias.nix
+++ zsh/alias.nix
@@ -10,14 +10,15 @@
   force = "git push --force-with-lease";
   g = "git";
   l = "ls -lah";
+  mux = "tmuxinator";
+  oni2 = "/Applications/Onivim2.app/Contents/MacOS/Oni2";
   p = "git add -p";
   pull = "git pull --rebase";
   push = "git push";
   root = "cd $(git rev-parse --show-toplevel)";
   s = "git status";
-  tmux = "tmux -u";
   t = "tmux -u";
+  tmux = "tmux -u";
   v = "nvim";
   yoda = "force";
-  oni2 = "/Applications/Onivim2.app/Contents/MacOS/Oni2";
 }
